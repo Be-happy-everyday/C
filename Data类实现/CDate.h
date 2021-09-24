4274dccb2e05f88a0d5b33f333cb09f56cabadf7
Be-happy-everyday <77975607+Be-happy-everyday@users.noreply.github.com>
2021-09-24 16:21:43 +0800

    定义错误更改

--- Data类实现/CDate.h
+++ Data类实现/CDate.h
@@ -5,9 +5,9 @@ using namespace std;
 class Date 
 {
 public:
-    Date(int y , int m , int d) {};
-    int days(int year, int month) {};
-    void NewDay() {};
+    Date(int y , int m , int d);
+    int days(int year, int month);
+    void NewDay();
     void display()
     {
         cout << year << "-" << month << "-" << day << endl;
